40d123dd66fa1af5a077c8532248a1ec2973c1c7
Guilherme <guilherme.artuzo@hotmail.com>
2020-10-19 22:30:53 -0300

    Classe Tabuleiro criada, iniciando implementação da classe

---
+++ JogoXadrez/tabuleiro/Peca.cs
@@ -0,0 +1,18 @@
+﻿namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao PosicaoTabuleiro { get; set; }
+        public Cor CorPeca { get; protected set; }
+        public int QtdMovimentos { get; protected set; }
+        public Tabuleiro Tab { get; protected set; }
+
+        public Peca(Posicao posicaoTabuleiro, Cor corPeca, Tabuleiro tab) 
+        {
+            PosicaoTabuleiro = posicaoTabuleiro;
+            CorPeca = corPeca;
+            QtdMovimentos = 0;
+            Tab = tab;
+        }
+    }
+}
